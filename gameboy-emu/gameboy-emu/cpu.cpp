5444611e465ad647e8196c83272729e291ed360f
Damir <damir.hodzic@stud.hslu.ch>
2019-07-26 13:57:51 +0200

    add opcode structure

--- gameboy-emu/gameboy-emu/cpu.cpp
+++ gameboy-emu/gameboy-emu/cpu.cpp
@@ -8,7 +8,7 @@ int main()
 	initialize();
 	loadRom("D:/Other/Gameboy/Game/Tetris (World).gb");
 	
-	for(int i = 0; i < 20; i++)
+	for(int i = 0; i < 10000; i++)
 	{
 		executeOpcode(fetchOpcode());
 	}
@@ -57,7 +57,7 @@ short int fetchOpcode()
 	
 	opcode = ram[programCounter] & 0x00FF;
 	programCounter++;
-	std::cout << "Opcode: " << std::hex << opcode << std::endl;
+	//std::cout << "Opcode: " << std::hex << opcode << std::endl;
 
 	return opcode;
 }
@@ -70,12 +70,8 @@ void executeOpcode(short int opcode)
 		{
 			break;
 		}
-		case 0xC3:
-		{
-			short int newAddressToJumpTo = (ram[programCounter + 1] << 8) + ram[programCounter];
-			programCounter = newAddressToJumpTo;
-			break;
-		}
+
+
 
 		/*LD nn, n*/
 		/*LD B, n*/
@@ -86,12 +82,12 @@ void executeOpcode(short int opcode)
 		/*LD C, n*/
 		case 0x0E:
 		{
-		
+
 		}
 		/*LD D, n*/
 		case 0x16:
 		{
-		
+
 		}
 		/*LD E, n*/
 		case 0x1E:
@@ -106,7 +102,7 @@ void executeOpcode(short int opcode)
 		/*LD L, n*/
 		case 0x2E:
 		{
-		
+
 		}
 
 
@@ -339,7 +335,7 @@ void executeOpcode(short int opcode)
 		{
 			break;
 		}
-		/*LD (HL),n*/ case 0x76:
+		/*LD (HL),n*/ case 0x36:
 		{
 			break;
 		}
@@ -353,7 +349,7 @@ void executeOpcode(short int opcode)
 			break;
 		}
 
-		/*LD A,(nn)*/ case 0xFA :
+		/*LD A,(nn)*/ case 0xFA:
 		{
 			break;
 		}
@@ -405,7 +401,7 @@ void executeOpcode(short int opcode)
 			break;
 		}
 
-		
+
 		/*LD A, (C)*/ case 0xF2:
 		{
 			break;
@@ -453,7 +449,7 @@ void executeOpcode(short int opcode)
 		}
 
 		/*LDH A,(n)*/
-		/*LD A,($FF00+n)*/ case 0xF0: 
+		/*LD A,($FF00+n)*/ case 0xF0:
 		{
 			break;
 		}
@@ -478,12 +474,1082 @@ void executeOpcode(short int opcode)
 
 		}
 
+		/*LDHL SP, n*/ case 0xF8: {
+			break;
+
+		}
+
+
+		/*LD (nn),SP*/ case 0x08: {
+			break;
+
+		}
+
+
+		/*PUSH AF*/ case 0xF5: {
+			break;
+
+		}
+
+
+		/*PUSH BC*/ case 0xC5: {
+			break;
+
+		}
+
+
+		/*PUSH DE*/ case 0xD5: {
+			break;
+
+		}
+
+
+		/*PUSH HL*/ case 0xE5: {
+			break;
+
+		}
+
+
+
+		/*POP AF*/ case 0xF1: {
+			break;
+
+		}
+
+
+		/*POP BC*/ case 0xC1: {
+			break;
+
+		}
+
+
+		/*POP DE*/ case 0xD1: {
+			break;
+
+		}
+
+
+		/*POP HL*/ case 0xE1: {
+			break;
+
+		}
+
+
+		/*ADD A,A*/ case 0x87:
+		{
+			break;
+		}
+		/*ADD A,B*/ case 0x80:
+		{
+			break;
+		}
+		/*ADD A,C*/ case 0x81:
+		{
+			break;
+		}
+		/*ADD A,D*/ case 0x82:
+		{
+			break;
+		}
+		/*ADD A,E*/ case 0x83:
+		{
+			break;
+		}
+		/*ADD A,H*/ case 0x84:
+		{
+			break;
+		}
+		/*ADD A,L*/ case 0x85:
+		{
+			break;
+		}
+		/*ADD A,(HL)*/ case 0x86:
+		{
+			break;
+		}
+		/*ADD A,#*/ case 0xC6:
+		{
+			break;
+		}
+
+
+
+		/*ADC A,A*/ case 0x8F:
+		{
+			break;
+		}
+		/*ADC A,B*/ case 0x88:
+		{
+			break;
+		}
+		/*ADC A,C*/ case 0x89:
+		{
+			break;
+		}
+		/*ADC A,D*/ case 0x8A:
+		{
+			break;
+		}
+		/*ADC A,E*/ case 0x8B:
+		{
+			break;
+		}
+		/*ADC A,H*/ case 0x8C:
+		{
+			break;
+		}
+		/*ADC A,L*/ case 0x8D:
+		{
+			break;
+		}
+		/*ADC A,(HL)*/ case 0x8E:
+		{
+			break;
+		}
+		/*ADC A,#*/ case 0xCE:
+		{
+			break;
+		}
+
+
+		/*SUB A*/ case 0x97:
+		{
+			break;
+		}
+		/*SUB B*/ case 0x90:
+		{
+			break;
+		}
+		/*SUB C*/ case 0x91:
+		{
+			break;
+		}
+		/*SUB D*/ case 0x92:
+		{
+			break;
+		}
+		/*SUB E*/ case 0x93:
+		{
+			break;
+		}
+		/*SUB H*/ case 0x94:
+		{
+			break;
+		}
+		/*SUB L*/ case 0x95:
+		{
+			break;
+		}
+		/*SUB (HL)*/ case 0x96:
+		{
+			break;
+		}
+		/*SUB #*/ case 0xD6:
+		{
+			break;
+		}
+
+
+		/*SBC A,A*/ case 0x9F:
+		{
+			break;
+		}
+		/*SBC A,B*/ case 0x98:
+		{
+			break;
+		}
+		/*SBC A,C*/ case 0x99:
+		{
+			break;
+		}
+		/*SBC A,D*/ case 0x9A:
+		{
+			break;
+		}
+		/*SBC A,E*/ case 0x9B:
+		{
+			break;
+		}
+		/*SBC A,H*/ case 0x9C:
+		{
+			break;
+		}
+		/*SBC A,L*/ case 0x9D:
+		{
+			break;
+		}
+		/*SBC A,(HL)*/ case 0x9E:
+		{
+			break;
+		}
+		/*SBC A,#*/ case 0xDE:
+		{
+			break;
+		}
+
+
+
+		/*AND A*/ case 0xA7:
+		{
+			break;
+		}
+		/*AND B*/ case 0xA0:
+		{
+			break;
+		}
+		/*AND C*/ case 0xA1:
+		{
+			break;
+		}
+		/*AND D*/ case 0xA2:
+		{
+			break;
+		}
+		/*AND E*/ case 0xA3:
+		{
+			break;
+		}
+		/*AND H*/ case 0xA4:
+		{
+			break;
+		}
+		/*AND L*/ case 0xA5:
+		{
+			break;
+		}
+		/*AND (HL)*/ case 0xA6:
+		{
+			break;
+		}
+		/*AND #*/ case 0xE6:
+		{
+			break;
+		}
+
+
+
+		/*OR A*/ case 0xB7:
+		{
+			break;
+		}
+		/*OR B*/ case 0xB0:
+		{
+			break;
+		}
+		/*OR C*/ case 0xB1:
+		{
+			break;
+		}
+		/*OR D*/ case 0xB2:
+		{
+			break;
+		}
+		/*OR E*/ case 0xB3:
+		{
+			break;
+		}
+		/*OR H*/ case 0xB4:
+		{
+			break;
+		}
+		/*OR L*/ case 0xB5:
+		{
+			break;
+		}
+		/*OR (HL)*/ case 0xB6:
+		{
+			break;
+		}
+		/*OR #*/ case 0xF6:
+		{
+			break;
+		}
+
+
+
+		/*XOR A*/ case 0xAF:
+		{
+			break;
+		}
+		/*XOR B*/ case 0xA8:
+		{
+			break;
+		}
+		/*XOR C*/ case 0xA9:
+		{
+			break;
+		}
+		/*XOR D*/ case 0xAA:
+		{
+			break;
+		}
+		/*XOR E*/ case 0xAB:
+		{
+			break;
+		}
+		/*XOR H*/ case 0xAC:
+		{
+			break;
+		}
+		/*XOR L */case 0xAD:
+		{
+			break;
+		}
+		/*XOR (HL)*/ case 0xAE:
+		{
+			break;
+		}
+		/*XOR * */ case 0xEE:
+		{
+			break;
+		}
+
+
+
+		/*CP A*/ case 0xBF:
+		{
+			break;
+		}
+		/*CP B*/ case 0xB8:
+		{
+			break;
+		}
+		/*CP C*/ case 0xB9:
+		{
+			break;
+		}
+		/*CP D*/ case 0xBA:
+		{
+			break;
+		}
+		/*CP E*/ case 0xBB:
+		{
+			break;
+		}
+		/*CP H*/ case 0xBC:
+		{
+			break;
+		}
+		/*CP L*/ case 0xBD:
+		{
+			break;
+		}
+		/*CP (HL)*/ case 0xBE:
+		{
+			break;
+		}
+		/*CP #*/ case 0xFE:
+		{
+			break;
+		}
+
+
+		/*INC A*/ case 0x3C:
+		{
+			break;
+		}
+		/*INC B*/ case 0x04:
+		{
+			break;
+		}
+		/*INC C*/ case 0x0C:
+		{
+			break;
+		}
+		/*INC D*/ case 0x14:
+		{
+			break;
+		}
+		/*INC E*/ case 0x1C:
+		{
+			break;
+		}
+		/*INC H*/ case 0x24:
+		{
+			break;
+		}
+		/*INC L*/ case 0x2C:
+		{
+			break;
+		}
+		/*INC (HL)*/ case 0x34:
+		{
+			break;
+		}
+
+
+
+		/*DEC A*/ case 0x3D:
+		{
+			break;
+		}
+		/*DEC B*/ case 0x05:
+		{
+			break;
+		}
+		/*DEC C*/ case 0x0D:
+		{
+			break;
+		}
+		/*DEC D*/ case 0x15:
+		{
+			break;
+		}
+		/*DEC E*/ case 0x1D:
+		{
+			break;
+		}
+		/*DEC H*/ case 0x25:
+		{
+			break;
+		}
+		/*DEC L*/ case 0x2D:
+		{
+			break;
+		}
+		/*DEC (HL)*/ case 0x35:
+		{
+			break;
+		}
+
+
+
+		/*ADD HL,BC*/ case 0x09:
+		{
+			break;
+		}
+		/*ADD HL,DE*/ case 0x19:
+		{
+			break;
+		}
+		/*ADD HL,HL*/ case 0x29:
+		{
+			break;
+		}
+		/*ADD HL,SP*/ case 0x39:
+		{
+			break;
+		}
+
+
+		/*ADD SP, #*/ case 0xE8:
+		{
+			break;
+		}
+
+
+
+		/*INC BC*/ case 0x03:
+		{
+			break;
+		}
+		/*INC DE*/ case 0x13:
+		{
+			break;
+		}
+		/*INC HL*/ case 0x23:
+		{
+			break;
+		}
+		/*INC SP*/ case 0x33:
+		{
+			break;
+		}
+
+
+
+		/*DEC BC*/ case 0x0B:
+		{
+			break;
+		}
+		/*DEC DE*/ case 0x1B:
+		{
+			break;
+		}
+		/*DEC HL*/ case 0x2B:
+		{
+			break;
+		}
+		/*DEC SP*/ case 0x3B:
+		{
+			break;
+		}
+
+
+
+
+		/*SWAP A*/ case 0xCB37:
+		{
+			break;
+		}
+		/*SWAP B*/ case 0xCB30:
+		{
+			break;
+		}
+		/*SWAP C*/ case 0xCB31:
+		{
+			break;
+		}
+		/*SWAP D*/ case 0xCB32:
+		{
+			break;
+		}
+		/*SWAP E*/ case 0xCB33:
+		{
+			break;
+		}
+		/*SWAP H*/ case 0xCB34:
+		{
+			break;
+		}
+		/*SWAP L*/ case 0xCB35:
+		{
+			break;
+		}
+		/*SWAP (HL)*/ case 0xCB36:
+		{
+			break;
+		}
+
+
+
+		/*DAA*/ case 0x27:
+		{
+			break;
+		}
+
+		/*CPL*/ case 0x2F:
+		{
+			break;
+		}
+
+
+		/*CCF*/ case 0x3F:
+		{
+			break;
+		}
+
+		/*SCF*/ case 0x37:
+		{
+			break;
+		}
+
+		/*HALT*/ case 0x76:
+		{
+			break;
+		}
+
+		/*STOP*/ case 0x1000:
+		{
+			break;
+		}
+
+
+		/*DI*/ case 0xF3:
+		{
+			break;
+		}
+
+
+
+		/*EI*/ case 0xFB:
+		{
+			break;
+		}
+
+		/*RLCA*/ case 0x07:
+		{
+			break;
+		}
+
+		/*RLA*/ case 0x17:
+		{
+			break;
+		}
+
+		/*RRCA*/ case 0x0F:
+		{
+			break;
+		}
+
+		/*RRA*/ case 0x1F:
+		{
+			break;
+		}
+
+		/*RLC A*/ case 0xCB07:
+		{
+			break;
+		}
+		/*RLC B*/ case 0xCB00:
+		{
+			break;
+		}
+		/*RLC C*/ case 0xCB01:
+		{
+			break;
+		}
+		/*RLC D*/ case 0xCB02:
+		{
+			break;
+		}
+		/*RLC E*/ case 0xCB03:
+		{
+			break;
+		}
+		/*RLC H*/ case 0xCB04:
+		{
+			break;
+		}
+		/*RLC L*/ case 0xCB05:
+		{
+			break;
+		}
+		/*RLC (HL)*/ case 0xCB06:
+		{
+			break;
+		}
+
+
+		/*RL A*/ case 0xCB17:
+		{
+			break;
+		}
+		/*RL B*/ case 0xCB10:
+		{
+			break;
+		}
+		/*RL C*/ case 0xCB11:
+		{
+			break;
+		}
+		/*RL D*/ case 0xCB12:
+		{
+			break;
+		}
+		/*RL E*/ case 0xCB13:
+		{
+			break;
+		}
+		/*RL H*/ case 0xCB14:
+		{
+			break;
+		}
+		/*RL L*/ case 0xCB15:
+		{
+			break;
+		}
+		/*RL (HL)*/ case 0xCB16:
+		{
+			break;
+		}
+
+
+
+		/*RRC A*/ case 0xCB0F:
+		{
+			break;
+		}
+		/*RRC B*/ case 0xCB08:
+		{
+			break;
+		}
+		/*RRC C*/ case 0xCB09:
+		{
+			break;
+		}
+		/*RRC D*/ case 0xCB0A:
+		{
+			break;
+		}
+		/*RRC E*/ case 0xCB0B:
+		{
+			break;
+		}
+		/*RRC H*/ case 0xCB0C:
+		{
+			break;
+		}
+		/*RRC L*/ case 0xCB0D:
+		{
+			break;
+		}
+		/*RRC (HL)*/ case 0xCB0E:
+		{
+			break;
+		}
+
+		/*RR A */case 0xCB1F:
+		{
+			break;
+		}
+		/*RR B */case 0xCB18:
+		{
+			break;
+		}
+		/*RR C */case 0xCB19:
+		{
+			break;
+		}
+		/*RR D */case 0xCB1A:
+		{
+			break;
+		}
+		/*RR E */case 0xCB1B:
+		{
+			break;
+		}
+		/*RR H */case 0xCB1C:
+		{
+			break;
+		}
+		/*RR L */case 0xCB1D:
+		{
+			break;
+		}
+		/*RR (HL)*/case 0xCB1E:
+		{
+			break;
+		}
+
+		/*SLA A*/ case 0xCB27:
+		{
+			break;
+		}
+		/*SLA B*/ case 0xCB20:
+		{
+			break;
+		}
+		/*SLA C*/ case 0xCB21:
+		{
+			break;
+		}
+		/*SLA D*/ case 0xCB22:
+		{
+			break;
+		}
+		/*SLA E*/ case 0xCB23:
+		{
+			break;
+		}
+		/*SLA H*/ case 0xCB24:
+		{
+			break;
+		}
+		/*SLA L*/ case 0xCB25:
+		{
+			break;
+		}
+		/*SLA (HL)*/ case 0xCB26:
+		{
+			break;
+		}
+
+		/*SRA A*/ case 0xCB2F:
+		{
+			break;
+		}
+		/*SRA B*/ case 0xCB28:
+		{
+			break;
+		}
+		/*SRA C*/ case 0xCB29:
+		{
+			break;
+		}
+		/*SRA D*/ case 0xCB2A:
+		{
+			break;
+		}
+		/*SRA E*/ case 0xCB2B:
+		{
+			break;
+		}
+		/*SRA H*/ case 0xCB2C:
+		{
+			break;
+		}
+		/*SRA L*/ case 0xCB2D:
+		{
+			break;
+		}
+		/*SRA (HL)*/ case 0xCB2E:
+		{
+			break;
+		}
+
+		/*SRL A*/ case 0xCB3F:
+		{
+			break;
+		}
+		/*SRL B*/ case 0xCB38:
+		{
+			break;
+		}
+		/*SRL C*/ case 0xCB39:
+		{
+			break;
+		}
+		/*SRL D*/ case 0xCB3A:
+		{
+			break;
+		}
+		/*SRL E*/ case 0xCB3B:
+		{
+			break;
+		}
+		/*SRL H*/ case 0xCB3C:
+		{
+			break;
+		}
+		/*SRL L*/ case 0xCB3D:
+		{
+			break;
+		}
+		/*SRL (HL)*/ case 0xCB3E:
+		{
+			break;
+		}
+
+
+		/*BIT b,A*/ case 0xCB47:
+		{
+			break;
+		}
+		/*BIT b,B*/ case 0xCB40:
+		{
+			break;
+		}
+		/*BIT b,C*/ case 0xCB41:
+		{
+			break;
+		}
+		/*BIT b,D*/ case 0xCB42:
+		{
+			break;
+		}
+		/*BIT b,E*/ case 0xCB43:
+		{
+			break;
+		}
+		/*BIT b,H*/ case 0xCB44:
+		{
+			break;
+		}
+		/*BIT b,L*/ case 0xCB45:
+		{
+			break;
+		}
+		/*BIT b,(HL)*/ case 0xCB46:
+		{
+			break;
+		}
+
+
+		/*SET b,A*/ case 0xCBC7:
+		{
+			break;
+		}
+		/*SET b,B*/ case 0xCBC0:
+		{
+			break;
+		}
+		/*SET b,C*/ case 0xCBC1:
+		{
+			break;
+		}
+		/*SET b,D*/ case 0xCBC2:
+		{
+			break;
+		}
+		/*SET b,E*/ case 0xCBC3:
+		{
+			break;
+		}
+		/*SET b,H*/ case 0xCBC4:
+		{
+			break;
+		}
+		/*SET b,L*/ case 0xCBC5:
+		{
+			break;
+		}
+		/*SET b,(HL)*/ case 0xCBC6:
+		{
+			break;
+		}
+
+		/*RES b,A*/ case 0xCB87:
+		{
+			break;
+		}
+		/*RES b,B*/ case 0xCB80:
+		{
+			break;
+		}
+		/*RES b,C*/ case 0xCB81:
+		{
+			break;
+		}
+		/*RES b,D*/ case 0xCB82:
+		{
+			break;
+		}
+		/*RES b,E*/ case 0xCB83:
+		{
+			break;
+		}
+		/*RES b,H*/ case 0xCB84:
+		{
+			break;
+		}
+		/*RES b,L*/ case 0xCB85:
+		{
+			break;
+		}
+		/*RES b,(HL)*/ case 0xCB86:
+		{
+			break;
+		}
+
+
+		/*JP nn*/ case 0xC3:
+		{
+			short int newAddressToJumpTo = (ram[programCounter + 1] << 8) + ram[programCounter];
+			programCounter = newAddressToJumpTo;
+			break;
+		}
+
+		/*JP NZ,nn*/ case 0xC2:
+		{
+			break;
+		}
+		/*JP Z,nn*/ case 0xCA:
+		{
+			break;
+		}
+		/*JP NC,nn*/ case 0xD2:
+		{
+			break;
+		}
+		/*JP C,nn*/ case 0xDA:
+		{
+			break;
+		}
+
+		/*JP (HL)*/ case 0xE9:
+		{
+			break;
+		}
+		/*JR n*/ case 0x18:
+		{
+			break;
+		}
+
+		/*JR NZ,**/ case 0x20:
+		{
+			break;
+		}
+		/*JR Z,* */case 0x28:
+		{
+			break;
+		}
+		/*JR NC,**/ case 0x30:
+		{
+			break;
+		}
+		/*JR C,* */case 0x38:
+		{
+			break;
+		}
+
+		/*CALL nn*/ case 0xCD:
+		{
+			break;
+		}
+
+		/*CALL NZ,nn*/ case 0xC4:
+		{
+			break;
+		}
+		/*CALL Z,nn*/ case 0xCC:
+		{
+			break;
+		}
+		/*CALL NC,nn*/ case 0xD4:
+		{
+			break;
+		}
+		/*CALL C,nn*/ case 0xDC:
+		{
+			break;
+		}
+
+		/*RST 00H*/ case 0xC7:
+		{
+			break;
+		}
+		/*RST 08H*/ case 0xCF:
+		{
+			break;
+		}
+		/*RST 10H*/ case 0xD7:
+		{
+			break;
+		}
+		/*RST 18H*/ case 0xDF:
+		{
+			break;
+		}
+		/*RST 20H*/ case 0xE7:
+		{
+			break;
+		}
+		/*RST 28H*/ case 0xEF:
+		{
+			break;
+		}
+		/*RST 30H*/ case 0xF7:
+		{
+			break;
+		}
+		/*RST 38H*/ case 0xFF:
+		{
+			break;
+		}
+
+		/*RET -/-*/ case 0xC9:
+		{
+			break;
+		}
+
+		/*RET NZ*/ case 0xC0:
+		{
+			break;
+		}
+		/*RET Z*/ case 0xC8:
+		{
+			break;
+		}
+		/*RET NC */case 0xD0:
+		{
+			break;
+		}
+		/*RET C*/ case 0xD8:
+		{
+			break;
+		}
+
+		/*RETI -/-*/ case 0xD9:
+		{
+			break;
+		}
 
 
 		default: 
 		{
 			std::cout << "Following opcode needs to be implemented: " << std::hex << opcode << std::endl;
-		}
+		};
 	}
 
 }
