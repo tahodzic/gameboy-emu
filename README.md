464e51e91b503fc7a7b21c96df780bad3a98c766
Damir <damir.hodzic@stud.hslu.ch>
2019-11-08 15:23:17 +0100

    add gif

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Not yet finished Gameboy emulator written in C++. With help from codeslinger.co.uk and r/EmuDev. Main documentation used for the Gameboy system are the [pandocs](http://bgb.bircd.org/pandocs.htm) and [the official Gameboy programming manual](https://archive.org/details/GameBoyProgManVer1.1).
 
 ![Tetris startup screen](screenshots/tetris_gbemu.png?raw=true "Tetris")
-![Kirby's Dreamland](screenshots/kirby.gif)
+![Kirby's Dreamland](screenshots/kirby.gif "Kirby's Dreamland")
+![Mario Yoshi](screenshots/marioyoshi.gif "Mario & Yoshi")
--- gameboy-emu/gameboy-emu/main.cpp
+++ gameboy-emu/gameboy-emu/main.cpp
@@ -12,9 +12,9 @@ int main()
 {
 
 	//if (!loadCartridge("D:/Other/Gameboy/Game/Tetris (World).gb"))
-	//if (!loadCartridge("D:/Other/Gameboy/Game/Mario_Yoshi.gb"))
+	if (!loadCartridge("D:/Other/Gameboy/Game/Mario_Yoshi.gb"))
 	//if (!loadCartridge("D:/Other/Gameboy/Game/Legend of Zelda, The - Link's Awakening (Germany).gb"))
-		if (!loadCartridge("D:/Other/Gameboy/Game/Kirbys Dream Land.gb"))
+		//if (!loadCartridge("D:/Other/Gameboy/Game/Kirbys Dream Land.gb"))
 			
 	//if (!loadCartridge("D:/Other/Gameboy/blarggs/gb-test-roms-master/cpu_instrs/cpu_instrs.gb"))
 	//if (!loadCartridge("D:/Other/Gameboy/blarggs/gb-test-roms-master/cpu_instrs/individual/02-interrupts.gb"))
